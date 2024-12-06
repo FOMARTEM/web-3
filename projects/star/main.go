2dad4138a3b468f3fd95842cef9824a9128f9b91
Artem Shahov <shahov9ad@gmail.com>
2024-12-06 18:56:46 +0300

    star

--- projects/star/main.go
+++ projects/star/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	str := ""
+	fmt.Scan(&str)
+	var str2 []rune
+	for i := 0; i < len(str)-1; i++ {
+		str2 = append(str2, rune(str[i]))
+		str2 = append(str2, '*')
+	}
+	str2 = append(str2, rune(str[len(str)-1]))
+	fmt.Println(string(str2))
 }
