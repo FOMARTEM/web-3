6ad981eaf0481a69bcc274121fcbed3f23a4941a
Artem Shahov <shahov9ad@gmail.com>
2024-12-06 18:59:00 +0300

    atoi

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// На вход подается целое число.
+// Необходимо возвести в квадрат каждую цифру числа и вывести получившееся число.
 func main() {
-	fmt.Println("Hello, world!!!")
+	number := ""
+	fmt.Scan(&number)
+	for i := 0; i < len(number); i++ {
+		temp := number[i] - 48
+		fmt.Print(temp * temp)
+	}
 }
